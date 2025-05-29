80fe3ec821ca34cc586125a5682d635ad4610c27
Francesco Zanini <zaninifrancesco03@gmail.com>
2025-05-29 17:33:18 +0200

    Add security configuration and test endpoint; integrate HTTP package for API calls

---
+++ Backend/minio-gallery/src/main/java/it/zaninifrancesco/minio_gallery/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package it.zaninifrancesco.minio_gallery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disabilita CSRF se non usi form HTML per le richieste di stato mutevole
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/test").permitAll() // Permetti l'accesso a /api/test senza autenticazione
+                                .anyRequest().authenticated() // Richiedi autenticazione per tutte le altre richieste
+                );
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
